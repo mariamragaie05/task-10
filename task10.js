35bb2de954fde18942e836fb2d338d82e829e609
Mariam Ragaie <mariamragaie05@gmail.com>
2024-08-29 10:55:36 +0300

    search array for duplicates

--- task10.js
+++ task10.js
@@ -20,3 +20,30 @@ const [
 ] = array;
 
 console.log(extract33, extractMoha);
+
+//Part 2
+const array2 = [
+  2,
+  4,
+  [22, "test"],
+  false,
+  null,
+  { a: 2 },
+  [22, "test"],
+  "null",
+];
+function hasDuplicates(array) {
+  const checked = new Set();
+  for (const element of array) {
+    const currentElement =
+      typeof element === "object" ? JSON.stringify(element) : String(element);
+    if (checked.has(currentElement)) {
+      return true;
+    }
+    checked.add(currentElement);
+  }
+  return false;
+}
+hasDuplicates(array2)
+  ? console.log("Array has duplicates")
+  : console.log("Array does not have duplicates");
